0e04e75651a552fd0ba02501b21bdd2e95e8f12c
sethah <shendrickson@cloudera.com>
2018-03-30 14:54:40 -0700

    add name scopes

--- models/tcn.py
+++ models/tcn.py
@@ -18,32 +18,33 @@ class CutRight(gluon.Block):
 
 
 class TemporalBlock(gluon.Block):
-    def __init__(self, k, d, out_channels, in_channels=1, dropout=0.4):
-        super(TemporalBlock, self).__init__()
-        self.in_channels = in_channels
-        self.kernel_size = k
-        self.dilation = d
-        self.out_channels = out_channels
+    def __init__(self, k, d, out_channels, in_channels=1, dropout=0.4, prefix=None):
+        super(TemporalBlock, self).__init__(prefix=prefix)
+        with self.name_scope():
+            self.in_channels = in_channels
+            self.kernel_size = k
+            self.dilation = d
+            self.out_channels = out_channels
 
-        self.net = gluon.nn.Sequential()
-        self.conv1 = gluon.nn.Conv1D(out_channels, k, in_channels=in_channels, dilation=d,
-                                     padding=d * (k - 1))
-        self.cut1 = CutRight(d * (k - 1))
-        self.bn1 = gluon.nn.BatchNorm()
-        self.relu1 = gluon.nn.Activation('relu')
-        self.dropout1 = gluon.nn.Dropout(dropout)
-        self.conv2 = gluon.nn.Conv1D(out_channels, k, in_channels=out_channels, dilation=d,
-                                     padding=d * (k - 1))
-        self.cut2 = CutRight(d * (k - 1))
-        self.bn2 = gluon.nn.BatchNorm()
-        self.relu2 = gluon.nn.Activation('relu')
-        self.dropout2 = gluon.nn.Dropout(dropout)
-        layers = [self.conv1, self.cut1, self.bn1, self.relu1, self.dropout1, self.conv2, self.cut2,
-                  self.bn2, self.relu2, self.dropout2]
-        for layer in layers:
-            self.net.add(layer)
-        self.relu = nn.Activation('relu')
-        self.downsample = nn.Conv1D(out_channels, 1) if in_channels != out_channels else None
+            self.net = gluon.nn.Sequential()
+            self.conv1 = gluon.nn.Conv1D(out_channels, k, in_channels=in_channels, dilation=d,
+                                         padding=d * (k - 1))
+            self.cut1 = CutRight(d * (k - 1))
+            self.bn1 = gluon.nn.BatchNorm()
+            self.relu1 = gluon.nn.Activation('relu')
+            self.dropout1 = gluon.nn.Dropout(dropout)
+            self.conv2 = gluon.nn.Conv1D(out_channels, k, in_channels=out_channels, dilation=d,
+                                         padding=d * (k - 1))
+            self.cut2 = CutRight(d * (k - 1))
+            self.bn2 = gluon.nn.BatchNorm()
+            self.relu2 = gluon.nn.Activation('relu')
+            self.dropout2 = gluon.nn.Dropout(dropout)
+            layers = [self.conv1, self.cut1, self.bn1, self.relu1, self.dropout1, self.conv2, self.cut2,
+                      self.bn2, self.relu2, self.dropout2]
+            for layer in layers:
+                self.net.add(layer)
+            self.relu = nn.Activation('relu')
+            self.downsample = nn.Conv1D(out_channels, 1) if in_channels != out_channels else None
 
     def forward(self, inputs):
         out = self.net(inputs)
@@ -52,24 +53,26 @@ class TemporalBlock(gluon.Block):
 
 
 class TemporalConvNet(gluon.Block):
-    def __init__(self, channel_list, in_channels, dropout=0.0, kernel_size=2):
-        super(TemporalConvNet, self).__init__()
-        self.net = nn.Sequential()
-        num_levels = len(channel_list)
-        for i in range(num_levels):
-            dilation = 2 ** i
-            in_channels = in_channels if i == 0 else channel_list[i - 1]
-            out_channels = channel_list[i]
-            self.net.add(
-                TemporalBlock(kernel_size, dilation, out_channels, in_channels=in_channels,
-                              dropout=dropout))
+    def __init__(self, channel_list, in_channels, dropout=0.0, kernel_size=2, prefix=None):
+        super(TemporalConvNet, self).__init__(prefix=prefix)
+        with self.name_scope():
+            self.net = nn.Sequential()
+            num_levels = len(channel_list)
+            for i in range(num_levels):
+                dilation = 2 ** i
+                in_channels = in_channels if i == 0 else channel_list[i - 1]
+                out_channels = channel_list[i]
+                self.net.add(
+                    TemporalBlock(kernel_size, dilation, out_channels, in_channels=in_channels,
+                                  dropout=dropout))
 
     def forward(self, inputs):
         return self.net(inputs)
 
 
 class TCN(gluon.Block):
-    def __init__(self, channel_list, in_channels, input_seq_len, output_dim, train_sequences=False):
+    def __init__(self, channel_list, in_channels, input_seq_len, output_dim, train_sequences=False,
+                 prefix=None):
         """
         :param channel_list: A list of output channels for each TemporalBlock layer.
         :param in_channels: The number of input sequences.
@@ -78,12 +81,14 @@ class TCN(gluon.Block):
         :param train_sequences: Whether to backprop the predictions for the entire sequence
                                 or just the last prediction.
         """
-        super(TCN, self).__init__()
+        super(TCN, self).__init__(prefix=prefix)
         self.input_seq_len = input_seq_len
         self.output_dim = output_dim
-        self.tcn = TemporalConvNet(channel_list, in_channels=in_channels)
-        self.dense = gluon.nn.Dense(output_dim)
         self.train_sequences = train_sequences
+        self.channel_list = channel_list
+        with self.name_scope():
+            self.tcn = TemporalConvNet(channel_list, in_channels=in_channels)
+            self.dense = gluon.nn.Dense(output_dim)
 
     def forward(self, inputs, exog=None):
         """
@@ -131,3 +136,4 @@ class TCN(gluon.Block):
         outputs = inp_buffer[:, input_seq_len:, :]
         assert outputs.shape == (pred_batch_size, predict_seq_len, self.output_dim)
         return outputs
+
